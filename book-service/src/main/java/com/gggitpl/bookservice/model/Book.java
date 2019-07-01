5d5a09914eba9e0b68586fe26b719e5140bbd957
gggitpl <960432397@qq.com>
2019-07-01 17:55:47 +0800

    update .c

---
+++ book-service/src/main/java/com/gggitpl/bookservice/model/Book.java
@@ -0,0 +1,14 @@
+package com.gggitpl.bookservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Book {
+
+  private Long id;
+  private String author;
+  private String title;
+
+}
--- user/src/main/java/com/gggitpl/user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.gggitpl.user.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.gggitpl.user.model.User;
-
-public interface UserService extends IService<User> {
-
-
-}
